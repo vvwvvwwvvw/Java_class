b0660b05fb931647cea38f5388136764ca9fcbe0
vvwvvwwvvw <wjddnr3304@gmail.com>
2023-11-30 13:25:57 +0900

    class

---
+++ src/ch01_variables/Ex01_Variables.java
@@ -0,0 +1,16 @@
+package ch01_variables;
+
+public class Ex01_Variables {
+
+	public static void main(String[] args) {
+		// 본인의 이름, 나이, 키, 주소, 전화번호를 입력할 수 있는 변수를 각각 선언 해봅시다.
+		String num1 = "남정욱";
+		int num2 = 30;
+		double num3 = 177.3;
+				String num4 = "인천 광역시 부평구 평천로 142번길17";
+				String num5 = "010 1234 5678";
+				
+				
+	}
+
+}
