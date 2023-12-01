d85c2077d611af5691b73ddcd4bc158330143e7d
vvwvvwwvvw <wjddnr3304@gmail.com>
2023-12-01 09:17:55 +0900

    class

--- src/ch02_operator/Ex01_ArithmeticOperator.java
+++ src/ch02_operator/Ex01_ArithmeticOperator.java
@@ -3,48 +3,81 @@ package ch02_operator;
 public class Ex01_ArithmeticOperator {
 
 	public static void main(String[] args) {
+		
+		//대입 연산자
+		int num1 = 10, num2 =7;
+		System.out.println("num1: "+ num1 + "num2: " + num2);
+		// 대입 연산자의 활용
+		num1 += num2; // num1= num1 + num2
+		System.out.println("num1: "+ num1 + "num2: " + num2); //num1:17 , num2:7
+		num1 += num2; // num1= num1 + num2
+		System.out.println("num1: "+ num1 + "num2: " + num2); //num1:24 , num2:7 
+		num1 -= num2; // num1= num1 + num2
+		System.out.println("num1: "+ num1 + "num2: " + num2); //num1:17 , num2:7 
+		num1 *= num2; // num1= num1 + num2
+		System.out.println("num1: "+ num1 + "num2: " + num2); //num1:94 , num2:7 
+		num1 /= num2; // num1= num1 + num2
+		System.out.println("num1: "+ num1 + "num2: " + num2); //num1:1 , num2:7 
+		//비교 연산자
+		
+//		System.out.println(10>5);
+//		System.out.println(10<5);
+//		boolean result = 77<=60;
+//		System.out.println(result);
+//		result = (8 ==100);
+//		System.out.println(result);
+//		result = false;
+//		boolean bool1 = true;
+//		//변수값을 다른 변수에 대입하는 것은 복사 개념
+//		result = bool1;
+//		System.out.println(result);
+//		System.out.println(bool1);
+		
+		
 		// 산술연산자
-        /**
-         * num1, num2 두개의 정수형 변수를 선언하고 값은 마음대로
-         * 두 변수의 +,-,*,/,% 연산결과를 print() 를 활용하여 출력해 봅시다
-         * 출력방법
-         * System.out.println(num1 + num2)
-         */
-		int num1 = 15;
-		int num2 = 3;
-		System.out.println("덧셈결과: " +num1+num2); // 앞쪽에 string 변수가 오면 연결에 의미로 받아들임
-		System.out.println("덧셈결과: " +(num1+num2));  // 앞쪽에 string 변수가 오게 하려면 ()를 사용하여 따로 계산하게 만들어 주어야함
-		System.out.println(num1+num2);
-		System.out.println(num1-num2);
-		System.out.println(num1*num2);
-		System.out.println(num1/num2);
-		System.out.println(num1%num2);
+//        /**
+//         * num1, num2 두개의 정수형 변수를 선언하고 값은 마음대로
+//         * 두 변수의 +,-,*,/,% 연산결과를 print() 를 활용하여 출력해 봅시다
+//         * 출력방법
+//         * System.out.println(num1 + num2)
+//         */
+//		int num1 = 15;
+//		int num2 = 3;
+//		System.out.println("덧셈결과: " +num1+num2); // 앞쪽에 string 변수가 오면 연결에 의미로 받아들임
+//		System.out.println("덧셈결과: " +(num1+num2));  // 앞쪽에 string 변수가 오게 하려면 ()를 사용하여 따로 계산하게 만들어 주어야함
+//		System.out.println(num1+num2);
+//		System.out.println(num1-num2);
+//		System.out.println(num1*num2);
+//		System.out.println(num1/num2);
+//		System.out.println(num1%num2);
+//		
+//		// 계산 결과를 담을 변수 활용
+//		int result = 0;
+//		result = num1 + num2;
+//		System.out.println(result);
+//		result = num1 - num2;
+//		System.out.println(result);
+//		result = num1 * num2;
+//		System.out.println(result);
+//		result = num1 / num2;
+//		System.out.println(result);
+//		result = num1 % num2;
+//		System.out.println(result);
+//		
+//		// 계산 결과 변수를 따로 선언
+//		
+//		int sumResult = num1 + num2;
+//		System.out.println(sumResult);
+//		int subResult = num1 - num2;
+//		System.out.println(subResult);
+//		int mulResult = num1 * num2;
+//		System.out.println(mulResult);
+//		int divResult = num1 / num2;
+//		System.out.println(divResult);
+//		int remainResult = num1 % num2;
+//		System.out.println(remainResult);
 		
-		// 계산 결과를 담을 변수 활용
-		int result = 0;
-		result = num1 + num2;
-		System.out.println(result);
-		result = num1 - num2;
-		System.out.println(result);
-		result = num1 * num2;
-		System.out.println(result);
-		result = num1 / num2;
-		System.out.println(result);
-		result = num1 % num2;
-		System.out.println(result);
 		
-		// 계산 결과 변수를 따로 선언
-		
-		int sumResult = num1 + num2;
-		System.out.println(sumResult);
-		int subResult = num1 - num2;
-		System.out.println(subResult);
-		int mulResult = num1 * num2;
-		System.out.println(mulResult);
-		int divResult = num1 / num2;
-		System.out.println(divResult);
-		int remainResult = num1 % num2;
-		System.out.println(remainResult);
 			
 
 	}
